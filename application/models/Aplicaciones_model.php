2639a6e25e70fd8ba9d77cd23f34b0a1f3b71620
Giovanni Estrada Alemán <vagio12345@gmail.com>
2012-06-06 10:59:43 -0500

    Correccion registro perfiles

--- application/models/Aplicaciones_model.php
+++ application/models/Aplicaciones_model.php
@@ -1,7 +1,12 @@
 <?php
-class Apliciones_model extends CI_Model {
+class Aplicaciones_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
+	
+	public function obtener_aplicaciones(){
+		$resultado = $this->db->get_where('CatAplicaciones', array('Status'=>1));
+		return $resultado;						
+	}
 }
 ?>
